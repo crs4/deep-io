fea524af3ec6d358efb4be89d9e58b2d9f37285c
josefransaenz <josefsaenz@gmail.com>
2020-10-21 11:32:28 +0200

    fixed a problem with event loops in threading

--- deep_io/deep_io.py
+++ deep_io/deep_io.py
@@ -135,7 +135,10 @@ class DeepIO:
             return
         self.running = True
         # run event loop
-        loop = asyncio.get_event_loop()
+        try:
+            loop = asyncio.get_event_loop()
+        except:
+            loop = asyncio.new_event_loop()
         try:
             loop.run_until_complete(self._start())
             #asyncio.run(asyncio.gather(demo1._start(), demo2._start()))
